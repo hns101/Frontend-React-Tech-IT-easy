b4068d8c29a007a6d35313e7b40ff83c545b7457
hns101 <hns101@live.nl>
2025-01-24 12:03:03 +0100

    setting up basic css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import './App.css';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+  return (<>
+    <header><h1>Tech It Easy Dashboard</h1></header>
+    <main>
+      <div id="outer-container-view">
+        <h2>Verkoopoverzicht</h2>
+        <div id="inner-container-card">
+          <div className="card" id="card-sold">
+            <h1 className="counter">33</h1>
+            <h3>Aantal verkocht producten</h3>
+          </div>
+          <div className="card" id="card-purchase">
+            <h1 className="counter">73</h1>
+            <h3>Aantal ingekochte producten</h3>
+          </div>
+          <div className="card" id="card-selling">
+            <h1 className="counter">40</h1>
+            <h3>Aantal te verkopen producten</h3>
+          </div>
+        </div>
+      </div>
+      <div id="top-product-container">
+      <h2>Best verkochte tv</h2>
+        <div className="card" id="top-product-card"></div>
+      </div>
+    </main>
+  </>)
 }
 
 export default App
